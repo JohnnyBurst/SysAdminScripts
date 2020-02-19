38952303858747db61a082a7ba195c37a0de0895
Via3 - Luciano G. Rodrigues <luciano.rodrigues@v3c.com.br>
2020-02-19 10:45:53 -0300

    Update PS_Remote_Check_WindowsUpdates.ps1

--- PS_Remote_Check_WindowsUpdates/PS_Remote_Check_WindowsUpdates.ps1
+++ PS_Remote_Check_WindowsUpdates/PS_Remote_Check_WindowsUpdates.ps1
@@ -1,10 +1,35 @@
-# -----------------------------------------------------------------------------------
-# Script para download e instalação das atualizações do windows - automatico
-# autor: luciano.grodrigues@live.com
-# -----------------------------------------------------------------------------------
+﻿<#
+
+.SYNOPSIS
+    This script checks a remote computer for pending windows updates.
+
+.DESCRIPTION
+	This script checks a remote computer for pending windows updates.
+	More than one computer can be checked, just separate them with a comma (see example below).
+	Note1: If more than one computer is checked, the whole process is run in parallel to speedup things.
+	Note2: Timeout to connecting to computers defaults to 10 seconds.
+
+.EXAMPLE
+    .\PS_Remote_Check_WindowsUpdates.ps1 -Computer server1,server2,server3
+
+
+.NOTES
+    Copyright (C) 2020  luciano.grodrigues@live.com
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+#>
 
 Param(
-	[Parameter(Mandatory=$True)] [String[]] $servers
+	[Parameter(Mandatory=$True)] [String[]] $computers
 )
 
 
@@ -14,16 +39,13 @@ $block_check_updates = {
 	$UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
 	$SearchResult = $UpdateSearcher.Search($SearchCriteria)
 
-	New-Object PSObject -Property @{Servidor=$env:computername; AtualizacoesDisponiveis=$SearchResult.Updates.Count}
+	#New-Object PSObject -Property @{Servidor=$env:computername; AtualizacoesDisponiveis=$SearchResult.Updates.Count}
+	Return $env:computername + "`t" + $SearchResult.Updates.Count
 }
 
 $ErrorActionPreference = "Stop"
-try{
-	$session = New-PSSession -Computername $servers
-	$Updates = Invoke-Command -Session $session -ScriptBlock $block_check_updates
-	Remove-PSSession $session
-	$Updates | Select-Object -Property Servidor,AtualizacoesDisponiveis
-}catch{
-	$_.Exception.Message
-	Remove-PSSession $session
-}
+Write-Host -ForegroundColor Yellow "Searching for updates"
+Write-Host "Server`t`tUpdates"
+
+$TimeOut = New-PSSessionOption -OpenTimeout 10000
+Invoke-Command -ComputerName $computers -ScriptBlock $block_check_updates -SessionOption $TimeOut
